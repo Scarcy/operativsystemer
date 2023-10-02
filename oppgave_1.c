dc3f7977b9aec43483218cdb1e80e9c37167a4a0
Ellefsen <monsoyen@gmail.com>
2023-10-02 21:20:22 +0200

    Makefile

--- oppgave_1.c
+++ oppgave_1.c
@@ -1,3 +1,9 @@
 #include <stdio.h>
 
-int main() {}
+int main() {
+  int sum = 0;
+  for (int i = 0; i <= 100; i++) {
+    sum += i * i;
+  }
+  printf("Summen av kvadratene av de 100 første heltallene er %d\n", sum);
+}
